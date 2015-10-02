3bb59f54a0e21a035ee019826a81ca49cd2aef35
brm <brm.ryd@gmail.com>
2015-10-02 09:04:55 +0700

    Speech math quiz

---
+++ speech-mathquiz/mathQuiz.js
@@ -0,0 +1,210 @@
+﻿// startup code
+var langs =
+[['Afrikaans',       ['af-ZA']],
+ ['Bahasa Indonesia',['id-ID']],
+ ['Bahasa Melayu',   ['ms-MY']],
+ ['Català',          ['ca-ES']],
+ ['Čeština',         ['cs-CZ']],
+ ['Deutsch',         ['de-DE']],
+ ['English',         ['en-AU', 'Australia'],
+                     ['en-CA', 'Canada'],
+                     ['en-IN', 'India'],
+                     ['en-NZ', 'New Zealand'],
+                     ['en-ZA', 'South Africa'],
+                     ['en-GB', 'United Kingdom'],
+                     ['en-US', 'United States']],
+ ['Español',         ['es-AR', 'Argentina'],
+                     ['es-BO', 'Bolivia'],
+                     ['es-CL', 'Chile'],
+                     ['es-CO', 'Colombia'],
+                     ['es-CR', 'Costa Rica'],
+                     ['es-EC', 'Ecuador'],
+                     ['es-SV', 'El Salvador'],
+                     ['es-ES', 'España'],
+                     ['es-US', 'Estados Unidos'],
+                     ['es-GT', 'Guatemala'],
+                     ['es-HN', 'Honduras'],
+                     ['es-MX', 'México'],
+                     ['es-NI', 'Nicaragua'],
+                     ['es-PA', 'Panamá'],
+                     ['es-PY', 'Paraguay'],
+                     ['es-PE', 'Perú'],
+                     ['es-PR', 'Puerto Rico'],
+                     ['es-DO', 'República Dominicana'],
+                     ['es-UY', 'Uruguay'],
+                     ['es-VE', 'Venezuela']],
+ ['Euskara',         ['eu-ES']],
+ ['Français',        ['fr-FR']],
+ ['Galego',          ['gl-ES']],
+ ['Hrvatski',        ['hr_HR']],
+ ['IsiZulu',         ['zu-ZA']],
+ ['Íslenska',        ['is-IS']],
+ ['Italiano',        ['it-IT', 'Italia'],
+                     ['it-CH', 'Svizzera']],
+ ['Magyar',          ['hu-HU']],
+ ['Nederlands',      ['nl-NL']],
+ ['Norsk bokmål',    ['nb-NO']],
+ ['Polski',          ['pl-PL']],
+ ['Português',       ['pt-BR', 'Brasil'],
+                     ['pt-PT', 'Portugal']],
+ ['Română',          ['ro-RO']],
+ ['Slovenčina',      ['sk-SK']],
+ ['Suomi',           ['fi-FI']],
+ ['Svenska',         ['sv-SE']],
+ ['Türkçe',          ['tr-TR']],
+ ['български',       ['bg-BG']],
+ ['Pусский',         ['ru-RU']],
+ ['Српски',          ['sr-RS']],
+ ['한국어',            ['ko-KR']],
+ ['中文',             ['cmn-Hans-CN', '普通话 (中国大陆)'],
+                     ['cmn-Hans-HK', '普通话 (香港)'],
+                     ['cmn-Hant-TW', '中文 (台灣)'],
+                     ['yue-Hant-HK', '粵語 (香港)']],
+ ['日本語',           ['ja-JP']],
+ ['Lingua latīna',   ['la']]];
+for (var i = 0; i < langs.length; i++) {
+  select_language.options[i] = new Option(langs[i][0], i);
+}
+select_language.selectedIndex = 6;
+updateCountry();
+select_dialect.selectedIndex = 6;
+showInfo('info_start');
+function updateCountry() {
+  for (var i = select_dialect.options.length - 1; i >= 0; i--) {
+    select_dialect.remove(i);
+  }
+  var list = langs[select_language.selectedIndex];
+  for (var i = 1; i < list.length; i++) {
+    select_dialect.options.add(new Option(list[i][1], list[i][0]));
+  }
+  select_dialect.style.visibility = list[1].length == 1 ? 'hidden' : 'visible';
+}
+
+var numberA = getRandomNumber();
+var numberB = getRandomNumber();
+
+
+var operators = [{
+        sign: "+",
+        method: function(a,b){ return a + b; }
+    },{
+        sign: "-",
+        method: function(a,b){ return a - b; }
+    }];
+
+var selectedOperator = Math.floor(Math.random()*operators.length);
+
+var s = operators[selectedOperator].sign;    //this will give you the sign
+var val = operators[selectedOperator].method(numberA, numberB);  //this will give you the answer
+
+var text = 'What is ' + numberA + s + numberB + '?';
+//var text = 'What is ' + numberA + ' + ' + numberB + '?';
+var response;
+ 
+// ask the problem
+ask(text, function (err, result) {
+    if (err) {
+        document.getElementById('result').innerHTML = 'No Answer.';
+    } else {
+        var answer = parseInt(result.transcript);
+	if (answer == val) {
+        //if (answer == numberA + numberB) {
+            response = 'Right! ' + numberA + s + numberB + ' is ' + answer + '.';
+	   // response = 'Right! ' + numberA + ' + ' + numberB + ' is ' + answer + '.';
+            speak(response);
+            document.getElementById('result').innerHTML = response;
+        } else {
+            response = 'Wrong! ' + numberA + s + numberB + ' is not ' + answer + '.';
+	    //response = 'Wrong! ' + numberA + ' + ' + numberB + ' is not ' + answer + '.';
+            speak(response);
+            document.getElementById('result').innerHTML = response;
+        }
+    }
+})
+ 
+// get random number between 1 - 10
+function getRandomNumber() {
+    return Math.floor((Math.random() * 10) + 1);
+}
+ 
+ 
+// ask a question and get an answer
+function ask(text, callback) {
+    // ask question
+    speak(text, function () {
+        // get answer
+        var recognition = new webkitSpeechRecognition();
+        //recognition.continuous = false;
+        //recognition.interimResults = false;
+	recognition.continuous = false;
+        recognition.interimResults = false;
+	
+ 
+        recognition.onend = function (e) {
+            if (callback) {
+                callback('no results');
+            }
+        };
+ 
+        recognition.onresult = function (e) {
+            // cancel onend handler
+            recognition.onend = null;
+            if (callback) {
+                callback(null, {
+                    transcript: e.results[0][0].transcript,
+                    confidence: e.results[0][0].confidence
+                });
+            }
+        }
+	recognition.lang = select_dialect.value;
+ 
+        // start listening
+        recognition.start();
+    });
+}
+ 
+ 
+// say a message
+function speak(text, callback) {
+    var u = new SpeechSynthesisUtterance();
+    u.text = text;
+    u.lang = 'en-US';
+    //u.lang = 'id-ID';
+    u.onend = function () {
+        if (callback) {
+            callback();
+        }
+    };
+ 
+    u.onerror = function (e) {
+        if (callback) {
+            callback(e);
+        }
+    };
+ 
+    speechSynthesis.speak(u);
+}
+
+function showInfo(s) {
+  if (s) {
+    for (var child = info.firstChild; child; child = child.nextSibling) {
+      if (child.style) {
+        child.style.display = child.id == s ? 'inline' : 'none';
+      }
+    }
+    info.style.visibility = 'visible';
+  } else {
+    info.style.visibility = 'hidden';
+  }
+}
+var current_style;
+function showButtons(style) {
+  if (style == current_style) {
+    return;
+  }
+  current_style = style;
+  copy_button.style.display = style;
+  email_button.style.display = style;
+  copy_info.style.display = 'none';
+  email_info.style.display = 'none';
+}
